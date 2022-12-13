edb74d48fed10720907a64fe378d97cbd2f03bfc
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-12-13 10:38:56 +0100

    Create basic structure of project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenceItem from "./Components/ExpenceItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <ExpenceItem></ExpenceItem>
+    </div>
+  );
+}
+
+export default App;
