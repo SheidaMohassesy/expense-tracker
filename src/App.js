4668784572d488ffb80ce7f468d7add7a7366f7e
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-12-14 18:19:00 +0100

    Improve css and components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 function App() {
   const expenses = [
     {
@@ -27,26 +27,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
